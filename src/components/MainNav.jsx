c11027ae8932ec2123e29ad1847117dee22e2633
Aleksandar Prlinčević <badprle@gmail.com>
2023-08-01 13:10:37 +0200

    fix: update navbar component name

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
-function Navbarr() {
+function MainNav() {
     return (
       <>
       <Navbar bg="light" data-bs-theme="light">
@@ -17,4 +17,4 @@ function Navbarr() {
     </> );
 }
 
-export default Navbarr;
+export default MainNav;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,83 +1,69 @@
 import TaskDetails from "./TaskDetails";
 import Footer from "./Footer";
 import EditTask from "./EditTask";
-import { useEffect, useState } from "react";
+import {useState} from "react";
 import Task from "../tasks.json";
 import ModalDelete from "../layouts/ModalDelete";
-import { Route, Routes } from "react-router-dom";
-import Navbarr from "./Navbar";
+import {Route, Routes} from "react-router-dom";
+import MainNav from "./MainNav";
 import AddTask from "./AddTask";
+
 const Tasks = () => {
-  const [tasks, setTasks] = useState(Task);
-  const [deletedId, setDeletedId] = useState("");
-  const [updateTask, setUpdateTask] = useState({
-    id: null,
-    title: "",
-    description: "",
-    status: null,
-    createdDate: null,
-  });
-  const updateUserTask = (type, value) => {
-    setUpdateTask({
-      ...updateTask,
-      id: updateTask.title !== "" ? updateTask.id : tasks.length + 1,
-      created_at:
-        updateTask.title !== ""
-          ? updateTask.created_at
-          : new Date().toISOString(),
-      [type]: value,
+    const [tasks, setTasks] = useState(Task);
+    const [deletedId, setDeletedId] = useState("");
+    const [updateTask, setUpdateTask] = useState({
+        id: null, title: "", description: "", status: null, createdDate: null,
     });
-  };
-  const onDelete = (id) => {
-    setDeletedId(id);
-  };
-  const onEdit = (id) => {
-    const task = tasks.filter((task) => task.id === id);
-    setUpdateTask(task[0]);
-    console.log(task[0])
-  };
-  return (
-    <>
-      <Navbarr />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <TaskDetails
-              tasks={tasks}
-              setTasks={setTasks}
-              onDelete={onDelete}
-              onEdit={onEdit}
-            />
-          }
-        />
-      <Route
-  path="/addTask"
-  element={
-    <AddTask
-      tasks={tasks}
-      setTasks={setTasks}
-      updateTask={updateTask}
-      setUpdateTask={updateUserTask}
-    />
-  }
-></Route>
-        <Route
-          path="/editTask"
-          element={
-            <EditTask
-              tasks={tasks}
-              setTasks={setTasks}
-              deletedId={deletedId}
-              updateTask={updateTask}
-              setUpdateTask={updateUserTask}
-            />
-          }
-        ></Route>
-      </Routes>
-      <ModalDelete tasks={tasks} deletedId={deletedId} setTasks={setTasks} />
-      <Footer />
-    </>
-  );
+    const updateUserTask = (type, value) => {
+        setUpdateTask({
+            ...updateTask,
+            id: updateTask.title !== "" ? updateTask.id : tasks.length + 1,
+            created_at: updateTask.title !== "" ? updateTask.created_at : new Date().toISOString(),
+            [type]: value,
+        });
+    };
+    const onDelete = (id) => {
+        setDeletedId(id);
+    };
+    const onEdit = (id) => {
+        const task = tasks.filter((task) => task.id === id);
+        setUpdateTask(task[0]);
+        console.log(task[0])
+    };
+    return (<>
+            <MainNav/>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<TaskDetails
+                        tasks={tasks}
+                        setTasks={setTasks}
+                        onDelete={onDelete}
+                        onEdit={onEdit}
+                    />}
+                />
+                <Route
+                    path="/addTask"
+                    element={<AddTask
+                        tasks={tasks}
+                        setTasks={setTasks}
+                        updateTask={updateTask}
+                        setUpdateTask={updateUserTask}
+                    />}
+                ></Route>
+                <Route
+                    path="/editTask"
+                    element={<EditTask
+                        tasks={tasks}
+                        setTasks={setTasks}
+                        deletedId={deletedId}
+                        updateTask={updateTask}
+                        setUpdateTask={updateUserTask}
+                    />}
+                ></Route>
+            </Routes>
+            <ModalDelete tasks={tasks} deletedId={deletedId} setTasks={setTasks}/>
+            <Footer/>
+        </>);
 };
 export default Tasks;
